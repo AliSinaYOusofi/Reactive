2ec7cb73ffaf1c6edc97f9d3a15b8405509e77ce
HajiAli <senayousofiali@gmail.com>
2024-02-06 16:57:53 +0500

    lists, touches, flex, navigation

---
+++ one_1/components/FlatListData.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import { Text, StyleSheet, FlatList, View } from 'react-native'
+
+
+// FLast list displays a list of structured data, which are
+// have the same properties, but the FlatList only renders the data
+// that is showing on the screen
+export default function FlatListData() {
+
+    return (
+        <View style={styles.container}>
+            <FlatList
+                data={[
+                    {
+                        name: "one",
+                        index: "0"
+                    },
+                    {
+                        name: "one",
+                        index: "0"
+                    },
+                    {
+                        name: "one",
+                        index: "0"
+                    },
+                    {
+                        name: "one",
+                        index: "0"
+                    },
+                ]}
+
+                renderItem={
+                    (item) => 
+                    <View style={styles.item}> 
+                        <Text> {item.name} </Text>
+                        <Text> {item.index} </Text>
+                    </View>}
+            />
+        </View>
+    )
+}
+
+const styles = StyleSheet({
+    
+    container: {
+        flex: 1,
+        paddingTop: 22,
+    },
+
+    item: {
+        padding: 10,
+        fontSize: 18,
+        height: 44,
+    },
+})
