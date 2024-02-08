ace1170cdc622bda3e7528644d6fe12f8146b62d
HajiAli <senayousofiali@gmail.com>
2024-02-08 17:11:53 +0500

    react-native community navigation

--- one_1/App.js
+++ one_1/App.js
@@ -1,20 +1,51 @@
-import { Navigation } from "react-native-navigation";
-import App from "./App";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import ScrollViews from './components/ScrollView';
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import FlatListData from './components/FlatListData';
+import SectionListData from './components/SectionListData';
 
-Navigation.registerComponent('com.myApp.WelcomeScreen', () => App);
+const Stack = createNativeStackNavigator()
 
-Navigation.events().registerAppLaunchedListener(() => {
-  Navigation.setRoot({
-    root: {
-      stack: {
-        children: [
-          {
-            component: {
-              name: 'com.myApp.WelcomeScreen'
-            }
-          }
-        ]
-      }
-    }
-  });
-})
+export default function App() {
+  
+  return (
+    <>
+      <StatusBar style="auto" />
+      
+      <NavigationContainer>
+        <Stack.Navigator>
+          
+          <Stack.Screen
+            name="homepage"
+            component={ScrollViews}
+            options={{title: "homepage"}}
+          />
+          
+          <Stack.Screen
+            name="List"
+            component={FlatListData}
+            options={{title: "Flat List Data"}}
+          />
+
+          <Stack.Screen
+            name="Section"
+            component={SectionListData}
+            options={{title: "Section list data"}}
+          />
+
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
