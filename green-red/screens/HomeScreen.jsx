0551a04ab9e81cb9ee7ec165797ca32c1d8b2b7a
HajiAli <senayousofiali@gmail.com>
2024-02-10 15:07:44 +0500

    amounts of take/give

---
+++ green-red/screens/HomeScreen.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import TotalExpenses from '../components/TotalExpenses'
+
+export default function HomeScreen() {
+    return (
+        <>
+            
+            <TotalExpenses />
+        </>
+    )
+}
